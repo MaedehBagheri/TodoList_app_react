66b9f9aeb2d0887d7d3f0ec03ca6c630203ec66d
maedeh <@maedehbagheri.com>
2023-01-29 13:43:11 +0330

    react commit

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+
+import styles from "./navbar.module.css";
+import { useProduct } from "./provider/providers";
+
+const Navbar =()=>{
+ const product=useProduct();
+ const totlaItems=product.filter((p)=> p.quantity>0).length;
+    return(
+<header className={styles.nav} >
+
+    <h2>mawland shopping</h2>
+    <span>{totlaItems}</span>
+</header>
+    )
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+
+
 import React from "react";
 import  ReactDOM from "react-dom";
-import App from "./app";
+import App2 from "./app2";
 import "./index.css"
+ 
 
-ReactDOM.render(<App/>,document.getElementById("root"));
 
+ReactDOM.render(<App2/>,document.getElementById("root"));
